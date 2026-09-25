a971f10245ace285223048bcccd60a2377b6194f
Mahlatselucky <luckymahlatse879@gmail.com>
2026-09-25 07:44:33 +0200

    Add GiftOfTheGivers.Helpers class library, publish to Azure Artifacts, and consume in Donate page (Section D)

---
+++ GiftOfTheGivers.Helpers/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace GiftOfTheGivers.Helpers
+{
+    public static class DonationHelper
+    {
+        // Formats a tax certificate number like TC-2026-000123
+        public static string FormatTaxCertificateNumber(int donationId, int year)
+        {
+            return $"TC-{year}-{donationId.ToString("D6")}";
+        }
+
+        // Calculates the total value of a list of donation amounts
+        public static decimal CalculateDonationTotal(List<decimal> donationAmounts)
+        {
+            decimal total = 0;
+            foreach (var amount in donationAmounts)
+            {
+                total += amount;
+            }
+            return total;
+        }
+    }
+}
--- GiftOfTheGivers1/Pages/Donate.cshtml.cs
+++ GiftOfTheGivers1/Pages/Donate.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GiftOfTheGivers.Helpers;
 
 namespace GiftOfTheGivers1.Pages
 {
     public class DonateModel : PageModel
     {
+        [BindProperty]
+        public decimal DonationAmount { get; set; }
+
+        public string GeneratedCertificateNumber { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            int dummyDonationId = 1;
+            GeneratedCertificateNumber = DonationHelper.FormatTaxCertificateNumber(dummyDonationId, 2026);
         }
     }
 }
